9557110782a1f462d6d97af36d1c33bc2ed3f255
MocarnyKrystian <146340720+MocarnyKrystian@users.noreply.github.com>
2023-09-28 14:01:58 +0200

    main

--- no.js
+++
@@ -1,9 +0,0 @@
-// var i = 10;
-// var clicks = 0;
-// function onclick() {
-// clicks += 1;
-// i=i - 1;
-// dane.textContent = i;
-// if(i <= 0)  {dane.textContent = "koniec"}
-// document.getElementById("clicks").innerHTML = clicks;
-// }
